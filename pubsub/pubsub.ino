e29de31ff86344aaa7a2776fe368c467babf28c2
Rizqirazkafi <rizqirazkafi56@gmail.com>
2023-12-21 16:30:57 +0700

    Add buzzer

--- pubsub/pubsub.ino
+++ pubsub/pubsub.ino
@@ -1,7 +1,7 @@
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <Arduino.h>
-#include <WiFiClientSecureBearSSL.h>
+// #include <ESP8266WiFi.h>
+// #include <WiFiClient.h>
+// #include <Arduino.h>
+// #include <WiFiClientSecureBearSSL.h>
 #include <WiFiManager.h>         // https://github.com/tzapu/WiFiManager
 #include <PubSubClient.h>
 #include <DHT.h>
@@ -10,12 +10,13 @@
 #define firePin D1
 #define DHT11PIN D2
 #define MQ2pin A0
-const int buzzpin = 14;
+int buzzer = D4;
 float smokeSensor;
 DHT dht11(DHT11PIN, DHT11);
 int resetPinState = 0;
 void connectWifi();
 void connect_mqtt();
+void saveConfigCallback ();
 String mqttBroker = "test.mosquitto.org";
 String randomTemp();
 WiFiClient client;
@@ -25,6 +26,9 @@ String pub_temp = wifiname + "/temp";
 String pub_humidity = wifiname + "/humidity";
 String pub_fireAlert = wifiname + "/fire";
 String pub_smokeValue = wifiname + "/smoke";
+char username[20];
+// char userpass[20];
+char room[50];
 void setup() {
   Serial.begin(9600);
   dht11.begin();
@@ -33,7 +37,7 @@ void setup() {
   pinMode(firePin, INPUT);
   connectWifi();
   mqtt.setServer(mqttBroker.c_str(), 1883);
-  pinMode(buzzpin, OUTPUT);
+  pinMode(buzzer, OUTPUT);
 }
 
 void loop() {
@@ -87,10 +91,22 @@ void loop() {
   if (flame_state == 0){
     mqtt.publish(pub_fireAlert.c_str(), "Fire");
     Serial.println("Fire");
-    tone(buzzpin, 1000);
+    delay(250);
+    digitalWrite(buzzer, HIGH);
+    delay(250);
+    digitalWrite(buzzer, LOW);
+    delay(250);
+    digitalWrite(buzzer, HIGH);
+    delay(250);
+    digitalWrite(buzzer, LOW);
+    delay(250);
+    digitalWrite(buzzer, HIGH);
+    delay(250);
+    digitalWrite(buzzer, LOW);
   } else {
     mqtt.publish(pub_fireAlert.c_str(), "No Fire");
     Serial.println("No Fire");
+    digitalWrite(buzzer, LOW);
   }
   delay(3000);
 }
@@ -110,13 +126,21 @@ void connect_mqtt(){
 }
 void connectWifi() {
   WiFi.hostname("siagadk1");
+  // WiFiManagerParameter add_username("name", "username", username, 20);
+  // WiFiManagerParameter add_userpass("pass", "password", userpass, 20);
+  // WiFiManagerParameter add_room("room", "room placement", room, 50);
   WiFiManager wm;
+  // wm.setSaveConfigCallback(saveConfigCallback);
+  // wm.addParameter(&add_username);
+  // wm.addParameter(&add_userpass);
+  // wm.addParameter(&add_room);
   // wm.resetSettings();
   // wm.setSaveConfigCallback(saveConfigCallback);
   // WiFiManagerParameter email("email", "Email Address", email, 50);
   // wm.addParameter(&email);
   bool res;
   res = wm.autoConnect("SIAGADK1", "merdeka45hebat");
+
   if (!res) {
     Serial.println("Failed to connect");
   } else {
@@ -127,4 +151,15 @@ void connectWifi() {
     Serial.println(WiFi.localIP());
     Serial.println(WiFi.gatewayIP());
   }
+  // strcpy(username, add_username.getValue());
+  // strcpy(userpass, add_userpass.getValue());
+  // strcpy(room, add_room.getValue());
+  // Serial.println("Value");
+  // Serial.println("\tusername: " + String(username));
+  // Serial.println("\tuserpass: " + String(userpass));
+  // Serial.println("\troom: " + String(room));
 }
+// void saveConfigCallback() {
+//   Serial.println("Should save config");
+//   shouldSaveConfig = true;
+// }
